5c2714cfbbb9383204165040fee31ae8dfa27e85
Jacob Greenway <jagreenway12@gmail.com>
2016-11-17 10:52:00 -0600

    convert to php, fix mail function

---
+++ about.php
@@ -0,0 +1,104 @@
+<html>
+<head>
+    <link rel="icon" href="resources/img/favicon.png">
+    <title>UTD Motorsports</title>
+    <meta name="viewport" content="width=device-width">
+    <!-- CSS References -->
+    <link href="resources/css/global.min.css" rel="stylesheet">
+    <link href="resources/css/about.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="menu-wrapper">
+    <a class="menu-toggle"><img src="resources/img/menu.png"></a>
+    <a href="/" class="mobile-logo"><img src="resources/img/logo.png"></a>
+    <ul class="tabs">
+        <a href="/"><img src="resources/img/utd-motorsports.png"></a>
+        <a href="/">
+            <li class="tab">Home</li>
+        </a>
+        <a href="/about">
+            <li class="active tab">About</li>
+        </a>
+        <a href="/the-car">
+            <li class="tab">The Car</li>
+        </a>
+        <a href="/sponsor">
+            <li class="tab">Sponsor</li>
+        </a>
+        <a href="/contact">
+            <li class="tab">Contact</li>
+        </a>
+    </ul>
+    <div class="social-media">
+        <a href="https://www.facebook.com/utdfsae" target="_blank"><span class="socicon-facebook"></span></a>
+    </div>
+</div>
+
+
+<div class="menu-buffer"></div>
+
+<div class="content-wrapper">
+    <h1>About UTD Motorsports</h1>
+    <img class="chapter-header" src="resources/img/header.png">
+    <p>UTD Motorsports is a group of dedicated and passionate UT Dallas students who will design, manufacture and
+        race an open wheel formula-style race car to compete in the national Formula SAE Championship.<br><br>We
+        recognize the need for students to take the concepts they learn in the classroom and apply them to
+        real-world situations. Our student team members gain invaluable experience with practical engineering
+        application and solving real-world challenges.<br><br>In addition to designing, building, testing and
+        racing the car, our members are responsible for all aspects of managing the race team. Including project
+        management, budgeting, sponsorship, procurement, and outreach.</p>
+
+
+    <h2 class="officer-header">Meet the Officers</h2>
+    <div class="officers">
+        <div class="row">
+            <div class="officer">
+                <div class="ruger"></div>
+                <span class="officer-tag">President</span>
+                <span>Ruger Carstens</span>
+            </div>
+            <div class="officer">
+                <div class="charles"></div>
+                <span class="officer-tag">Vice President</span>
+                <span>Charles VanDergrift</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="officer">
+                <div class="arye"></div>
+                <span class="officer-tag">Head of Design</span>
+                <span>Arye Levi</span>
+            </div>
+            <div class="officer">
+                <div class="david"></div>
+                <span class="officer-tag">Head of Sponsorship</span>
+                <span>David Azpe</span>
+            </div>
+            <div class="officer">
+                <div class="sharon"></div>
+                <span class="officer-tag">Secretary</span>
+                <span>Sharon Cho</span>
+            </div>
+        </div>
+    </div>
+
+    <h1>About Formula SAE</h1>
+    <p>The Formula SAE competition is organized by SAE International (formerly Society of Automotive Engineers).
+        SAE is a global association of more than 138,000 engineers and related technical experts in the
+        aerospace, automotive, and commercial vehicle industries.<br><br>SAE International is the leader in
+        connecting and educating engineers while promoting, developing, and advancing aerospace, commercial
+        vehicle, and automotive engineering. Formula SAE is the largest of the SAE-sponsored student design
+        competitions within its Collegiate Design Series.<br><br>During the four-day event, UTD Motorsports will
+        compete among a field of over 100 schools from around the world. Our race car will be judged on the
+        vehicle's design, cost, innovation, safety, and performance, as well as our team's ability to present
+        the engineering concepts used to develop the final design.</p>
+
+    <p class="footnote">If you are interesting in learning more about our team or want your company to sponsor
+        us, please contact us <a href="/contact">here</a>.
+    </p>
+</div>
+</body>
+<!-- JQuery & JS References -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="resources/js/menu.min.js"></script>
+</html>
